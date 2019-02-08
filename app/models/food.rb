f713fe2228c66dabdfeecfffef01f4c79ba23ca9
tanin <t.banihashemi@gmailcom>
2019-02-07 23:33:55 -0600

    Created 10 different foods in DB

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,4 +1,4 @@
 class Food < ApplicationRecord
-    validates :name, :ingredient, :price, presence:true
+    validates :dish, :ingredient, :price, presence:true
     validates :price, numericality:true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Food.destroy_all
+
+10.times do
+    Food.create(
+        dish:Faker::Food.unique.dish,
+        ingredient:Faker::Food.ingredient,
+        spice:Faker::Food.spice,
+        price:Faker::Commerce.price(range = 10..40.0, as_string = false)
+    )
+end
+
+puts "beers in database #{Food.count}"
